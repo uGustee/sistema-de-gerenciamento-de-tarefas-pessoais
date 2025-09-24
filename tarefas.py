d5cc8706a7f12fefd481b49df4be4da11b8cdc0c
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-09-23 22:25:56 -0300

    Update tarefas.py

--- tarefas.py
+++ tarefas.py
@@ -1,38 +1,26 @@
-def cadastrar_tarefa():
-    id_tarefa = input("ID tarefa: ")
-    titulo = input("Título: ")
-    descricao = input("Descrição: ")
-    data_criacao = input("Data de criação: ")
-    vencimento = input("Data de vencimento: ")
-    prioridade = input("Prioridade (número): ")
-    categoria = input("Categoria: ")
-    id_usuario = input("ID do usuário dono: ")
-    id_projeto = input("ID do projeto: ")
-    cor = input("Cor (hexadecimal ou nome): ")
-    criado_em = input("Criado em: ")
+print("Cadastro de Tarefa")
 
-    tarefa = {
-        "id_tarefa": id_tarefa,
-        "titulo": titulo,
-        "descricao": descricao,
-        "data_criacao": data_criacao,
-        "vencimento": vencimento,
-        "prioridade": prioridade,
-        "categoria": categoria,
-        "id_usuario": id_usuario,
-        "id_projeto": id_projeto,
-        "cor": cor,
-        "criado_em": criado_em
-    }
-    return tarefa
+id_tarefas = input("ID da tarefa: ")
+titulo_tarefa = input("Título: ")
+descricao_tarefa = input("Descrição: ")
+data_criacao = input("Data de criação: ")
+data_vencimento = input("Data de vencimento: ")
+prioridade = input("Prioridade (número): ")
+categoria = input("Categoria: ")
+id_usuario = input("ID do usuário: ")
+id_projeto = input("ID do projeto: ")
+cor_projeto = input("Cor do projeto: ")
+criado_em = input("Criado em: ")
 
-def mostrar_tarefa(t):
-    print("Tarefa:", t["id_tarefa"])
-    print(" Título:", t["titulo"])
-    print(" Descrição:", t["descricao"])
-    print(" Vencimento:", t["vencimento"])
-    print(" Prioridade:", t["prioridade"])
-    print(" Categoria:", t["categoria"])
-    print(" Usuário:", t["id_usuario"])
-    print(" Projeto:", t["id_projeto"])
-    print(" Cor:", t["cor"])
+print("\n--- Tarefa cadastrada ---")
+print("ID:", id_tarefas)
+print("Título:", titulo_tarefa)
+print("Descrição:", descricao_tarefa)
+print("Criação:", data_criacao)
+print("Vencimento:", data_vencimento)
+print("Prioridade:", prioridade)
+print("Categoria:", categoria)
+print("Usuário:", id_usuario)
+print("Projeto:", id_projeto)
+print("Cor:", cor_projeto)
+print("Criado em:", criado_em)
