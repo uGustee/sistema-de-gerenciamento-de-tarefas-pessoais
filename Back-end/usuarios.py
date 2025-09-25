e1b3d3b039f27183aa8c391633d9ea2228ad3c6e
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-09-25 14:56:25 -0300

    Update usuarios.py

--- Back-end/usuarios.py
+++ Back-end/usuarios.py
@@ -1,18 +1,19 @@
-nome = input("Digite o nome do usuário: ")
+def formatar_telefone(telefone):
+    return f"({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}"
 
-cpf = input("Digite o CPF do usuário (apenas números): ")
-cpf = cpf[:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:]
+def formatar_cpf(cpf):
+    return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
 
-telefone = input("Digite o telefone do usuário (apenas números): ")
-telefone = "(" + telefone[:2] + ")" + telefone[2:7] + "-" + telefone[7:]
+id_usuario = input("Digite o ID do usuário: ")
+nome = input("Digite o nome: ")
+email = input("Digite o email: ")
+senha = input("Digite a senha: ")
+cpf = input("Digite o CPF (somente números, ex: 12345678900): ")
+telefone = input("Digite o telefone (somente números com DDD, ex: 41999999999): ")
 
-email = input("Digite o e-mail do usuário: ")
-
-rua = input("Digite a rua: ")
-numero = input("Digite o número: ")
-bairro = input("Digite o bairro: ")
-cidade = input("Digite a cidade: ")
-estado = input("Digite o estado (sigla, ex: PR): ")
-
-cep = input("Digite o CEP (apenas números): ")
-cep = cep[:5] + "-" + cep[5:]
+print("\n--- Dados do Usuário ---")
+print("ID:", id_usuario)
+print("Nome:", nome)
+print("Email:", email)
+print("CPF:", formatar_cpf(cpf))
+print("Telefone:", formatar_telefone(telefone))
