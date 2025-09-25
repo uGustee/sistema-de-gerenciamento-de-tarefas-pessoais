41f1c22b0973ed2ce685109e1c247282c620723c
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-09-25 14:32:43 -0300

    Update usuarios.py

--- Back-end/usuarios.py
+++ Back-end/usuarios.py
@@ -7,3 +7,12 @@ telefone = input("Digite o telefone do usuário (apenas números): ")
 telefone = "(" + telefone[:2] + ")" + telefone[2:7] + "-" + telefone[7:]
 
 email = input("Digite o e-mail do usuário: ")
+
+rua = input("Digite a rua: ")
+numero = input("Digite o número: ")
+bairro = input("Digite o bairro: ")
+cidade = input("Digite a cidade: ")
+estado = input("Digite o estado (sigla, ex: PR): ")
+
+cep = input("Digite o CEP (apenas números): ")
+cep = cep[:5] + "-" + cep[5:]
